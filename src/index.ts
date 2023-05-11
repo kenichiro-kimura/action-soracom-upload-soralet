26f95a8a7cab9994452bfcef7d6415d90b7d6b9c
Ken'ichirou Kimura <39895593+kenichiro-kimura@users.noreply.github.com>
2023-05-11 17:53:05 +0900

    Add lint and CI workflow (#7)
    * [add] add lint

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,53 @@
+import * as fs from "fs";
+import * as core from "@actions/core";
 import * as soracom from "./soracom";
-import * as fs from 'fs';
-import * as core from '@actions/core';
 
-async function main(){
-    const authApi = new soracom.AuthApi();
-    const soraletApi = new soracom.SoraletApi();
-    const authRequest = new soracom.AuthRequest();
-    authRequest.authKey = core.getInput('soracom_auth_key', { required: true}); 
-    authRequest.authKeyId = core.getInput('soracom_auth_key_id', { required: true}); 
-    const soraletId: string = core.getInput('soracom_soralet_id', { required: true});
-    const soraletFilename: string = core.getInput('soracom_soralet_filename', { required: true});
-    const coverage: string = core.getInput('soracom_coverage', { required: false}) ? core.getInput('soracom_coverage', { required: false}) : "jp";
+async function main() {
+    const authApi = new soracom.API.AuthApi();
+    const soraletApi = new soracom.API.SoraletApi();
+    const authRequest = new soracom.Model.AuthRequest();
+    authRequest.authKey = core.getInput("soracom_auth_key", { required: true });
+    authRequest.authKeyId = core.getInput("soracom_auth_key_id", { required: true });
+    const soraletId: string = core.getInput("soracom_soralet_id", { required: true });
+    const soraletFilename: string = core.getInput("soracom_soralet_filename", { required: true });
+    const coverage: string = core.getInput("soracom_coverage", { required: false }) ? core.getInput("soracom_coverage", { required: false }) : "jp";
     const endpoint: string = coverage === "g" ? "https://g.api.soracom.io/v1" : "https://api.soracom.io/v1";
 
-    authApi.basePath = soraletApi.basePath = endpoint;
+    authApi.basePath = endpoint;
+    soraletApi.basePath = endpoint;
 
     try {
         const authResult = await authApi.auth(authRequest);
         const apiKey = authResult.body.apiKey ? authResult.body.apiKey : "";
         const apiToken = authResult.body.token ? authResult.body.token : "";
-    
-        authApi.setApiKey(soracom.AuthApiApiKeys.api_key, apiKey);
-        authApi.setApiKey(soracom.AuthApiApiKeys.api_token,apiToken);
 
-        soraletApi.setApiKey(soracom.SoraletApiApiKeys.api_key, apiKey);
-        soraletApi.setApiKey(soracom.SoraletApiApiKeys.api_token,apiToken);
+        authApi.setApiKey(soracom.API.AuthApiApiKeys.api_key, apiKey);
+        authApi.setApiKey(soracom.API.AuthApiApiKeys.api_token, apiToken);
+
+        soraletApi.setApiKey(soracom.API.SoraletApiApiKeys.api_key, apiKey);
+        soraletApi.setApiKey(soracom.API.SoraletApiApiKeys.api_token, apiToken);
 
         try {
             await soraletApi.getSoralet(soraletId);
         } catch (error) {
-            const createSoraletRequest = new soracom.CreateSoraletRequest();
+            const createSoraletRequest = new soracom.Model.CreateSoraletRequest();
             createSoraletRequest.soraletId = soraletId;
-            const createSoraletResult = await soraletApi.createSoralet(createSoraletRequest);
+            await soraletApi.createSoralet(createSoraletRequest);
         }
-        const requestFile : soracom.RequestFile = fs.createReadStream(soraletFilename);
-        const uploadResult = await soraletApi.uploadSoraletCode(soraletId,requestFile,"application/octet-stream");
+        const requestFile : soracom.API.RequestFile = fs.createReadStream(soraletFilename);
+        const uploadResult = await soraletApi.uploadSoraletCode(soraletId, requestFile, "application/octet-stream");
         console.log(uploadResult.body);
-        core.setOutput("result",uploadResult.body);
-    } catch(error) {
+        core.setOutput("result", uploadResult.body);
+    } catch (error) {
         try {
-            const logoutResult = await authApi.logout();
+            await authApi.logout();
         } catch (e) {
             console.error(e);
         }
         let errorMessage: string = "";
         if (typeof error === "string") {
             errorMessage = error;
-        } else if (error instanceof soracom.HttpError) {
+        } else if (error instanceof soracom.API.HttpError) {
             errorMessage = JSON.stringify(error.body);
         } else if (error instanceof Error) {
             errorMessage = JSON.stringify(error.message);
@@ -55,4 +56,14 @@ async function main(){
     }
 }
 
-main();
+main().catch((error) => {
+    let errorMessage: string = "";
+    if (typeof error === "string") {
+        errorMessage = error;
+    } else if (error instanceof soracom.API.HttpError) {
+        errorMessage = JSON.stringify(error.body);
+    } else if (error instanceof Error) {
+        errorMessage = JSON.stringify(error.message);
+    }
+    core.setFailed(errorMessage);
+});
--- src/soracom.ts
+++ src/soracom.ts
@@ -1,3 +1,5 @@
 // This is the entrypoint for the package
-export * from './api/apis';
-export * from './model/models';
+import * as API from "./api/apis";
+import * as Model from "./model/models";
+
+export { API, Model };
